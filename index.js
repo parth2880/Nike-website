0d1b662c78de29c587934c7f156da05891e5d30a
Parth Sharma <112793762+parth2880@users.noreply.github.com>
2023-02-06 13:36:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let new_featured = document.querySelector("#id-nf");
+console.log(new_featured);
+$(document).ready(function(){
+    $("#id-nf").hover(function(){
+        $("#id-nf-div").slideToggle(1000);
+    })
+})
+
+
+
+
+
+
+
+
