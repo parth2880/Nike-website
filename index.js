e2a9fdb9171398b82a463217938b204ab2cea2f4
Parth Sharma <112793762+parth2880@users.noreply.github.com>
2023-02-06 16:53:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let new_featured = document.querySelector("#id-nf");
-console.log(new_featured);
-$(document).ready(function(){
-    $("#id-nf").hover(function(){
-        $("#id-nf-div").slideToggle(1000);
-    })
-})
+// let new_featured = document.querySelector("#id-nf");
+// console.log(new_featured);
+// $(document).ready(function(){
+//     $("#id-nf").hover(function(){
+//         $("#id-nf-div").slideToggle(1000);
+//     })
+// })
 
 
 
